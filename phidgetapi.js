0882a1d65ad427a9161fec06698f80a8d44e7f51
Brandon Nozaki Miller <brandon@diginow.it>
2015-08-09 18:39:13 -0700

    working on interfacekit modul\e

--- phidgetapi.js
+++ phidgetapi.js
@@ -7,11 +7,13 @@
  */
 var Phidget = require('./lib/Phidget.js').Phidget;
 var Manager = require('./lib/Manager.js').Manager;
-var GPS = require('./lib/GPS.js').Phidget;
+var GPS     = require('./lib/GPS.js').Phidget;
+var InterfaceKit = require('./lib/InterfaceKit.js').Phidget;
 
 exports.Phidget = Phidget;
 exports.Manager = Manager;
 exports.GPS     = GPS;
+exports.InterfaceKit=InterfaceKit;
 
 //backwards compatibilirty only below
 exports.phidget = Phidget;
