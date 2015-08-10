8e511a4d20fcaec5765a3927981b9ff3ea7e34be
Brandon Nozaki Miller <brandon@diginow.it>
2015-08-10 13:31:34 -0700

    prepped for publish

--- examples/InterfaceKit.js
+++ examples/InterfaceKit.js
@@ -2,6 +2,21 @@ var Phidget = require('../phidgetapi').InterfaceKit;
 
 var IK=new Phidget;
 
-
 IK.phidget.connect();
 
+
+IK.observeOutputs(outputs);
+
+function outputs(changes){
+   //console.log(changes)  
+}
+
+IK.phidget.on(
+    'phidgetReady',
+    setOutput
+)
+
+function setOutput(){
+    IK.outputs[0]=1;
+    //console.log(IK.outputs)
+}
