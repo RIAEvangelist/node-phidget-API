49dade6e185e86281be4dd877e9694f0c110c325
Brandon Nozaki Miller <brandon@diginow.it>
2015-08-10 19:34:28 -0700

    completed InterfaceKit module and examples

--- examples/InterfaceKit.js
+++ examples/InterfaceKit.js
@@ -6,17 +6,73 @@ IK.phidget.connect();
 
 
 IK.observeOutputs(outputs);
-
-function outputs(changes){
-   //console.log(changes)  
-}
+IK.observeInputs(inputs);
+IK.observeSensors(sensors);
+IK.observeRawSensors(rawSensors);
 
 IK.phidget.on(
     'phidgetReady',
-    setOutput
-)
+    init
+);
 
-function setOutput(){
-    IK.outputs[0]=1;
-    //console.log(IK.outputs)
+function init(){
+    //do some initial set up here... like blinking an led.
+    setInterval(
+        function(){
+            IK.outputs[0]=(!IK.outputs[0])? 1:0; //if output is off turn it on || else turn it off
+        },
+        1000
+    )
+}
+
+
+function sensors(changes){
+    for(var i in changes){
+        var change=changes[i];
+        //see specific info about each change
+        //console.log(change);
+    }
+
+    //see updated IK data after all changes
+    console.log('Sensors',IK.sensors);
+
+    //do something with the sensor info like turn another LED on.
+    if(IK.sensors[7]>500){
+        IK.outputs[1]=1;
+    }else{
+        IK.outputs[1]=0;
+    }
+}
+
+function rawSensors(changes){
+    for(var i in changes){
+        var change=changes[i];
+        //see specific info about each change
+        //console.log(change);
+    }
+
+    //see updated IK data after all changes
+    console.log('Raw Sensors',IK.rawSensors);
+}
+
+function outputs(changes){
+    for(var i in changes){
+        var change=changes[i];
+        //see specific info about each change
+        //console.log(change);
+    }
+
+    //see updated IK data after all changes
+    console.log('Outputs',IK.outputs);
+}
+
+function inputs(changes){
+    for(var i in changes){
+        var change=changes[i];
+        //see specific info about each change
+        //console.log(change);
+    }
+
+    //see updated IK data after all changes
+    console.log('Inputs',IK.inputs);
 }
