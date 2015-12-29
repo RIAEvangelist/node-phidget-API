bfd8416d8a6e1bfe6c31b1e2fdccba10b9b6cf04
mario solorzano <msolorzano@2-sight.com>
2015-12-28 16:42:21 -0800

    completed PhidgetAnalog and example

--- examples/analog.js
+++ examples/analog.js
@@ -4,7 +4,26 @@ var analog=new Phidget();
 analog.connect();
 
 analog.whenReady(init)
+analog.observeVoltage(voltage);
 
 function init(){
-    console.log(analog.numberOfOutputs);
+    setInterval(
+        function(){
+            if(analog.voltage[0] === 0){
+                analog.voltage[0] = 5;
+            }else{
+                analog.voltage[0] = 0;
+            }
+            if(analog.enabled === 0){
+                analog.enabled = 1;
+            }else{
+                analog.enabled = 0;
+            }
+        },
+        5000
+    );
+}
+
+function voltage(changes){
+    console.log(analog.voltage);
 }
