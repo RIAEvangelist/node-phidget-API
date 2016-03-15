373e1eb2f1e1c17953fa88a74f369fe14370b398
seesleestak <calvin.cieslak@gmail.com>
2016-03-14 18:10:31 -0700

    IR update

--- examples/IR.js
+++ examples/IR.js
@@ -12,10 +12,13 @@ function init(){
     IR.observe(main);
 }
 
+//IR.transmit='20df10ef,32,1';
+//console.log('on');
+
 function main(changes){
     var code=IR.readRaw;
     console.log('IR code received: ', code);
 
-    IR.transmit(code);
+    IR.transmit=IR.readRaw;
     console.log('IR code transmitted: ', IR.transmit);
 }
