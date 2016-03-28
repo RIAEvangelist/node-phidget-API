f23360398bbd606a1fdcdf7fa8a29c4dbf4cb574
seesleestak <calvin.cieslak@gmail.com>
2016-03-28 15:49:29 -0700

    IR update

--- examples/IR.js
+++ examples/IR.js
@@ -1,5 +1,9 @@
-var Phidget = require('../phidgetapi').IR;
-var IR = new Phidget();
+'use strict';
+
+const Phidget = require('../phidgetapi').IR;
+const IR = new Phidget();
+
+//Power On/Off learn code: 200000000200000002000000e4a4010034020000502300009e110000340200009d060000340200004202000050230000cf0800003402000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000709400003200000020df10ef
 
 IR.connect();
 console.log('Successfully connected');
@@ -15,7 +19,8 @@ function init(){
 function main(changes){
     console.log('IR code received: ', IR.readRaw);
 
-    code='20df10ef,32,1';
-    IR.Transmit=code;
-    //console.log('IR code transmitted: ');
+    let code='20df10ef,32,0';
+    code=code.split(',')[0];
+
+    IR.transmit=code;
 }
